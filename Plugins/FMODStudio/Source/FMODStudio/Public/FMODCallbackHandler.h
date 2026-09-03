649313e303c54591b41600fbf59abea1cd16bea0
Natalia <n.podgorska@gmail.com>
2026-09-03 07:07:11 +0100

    Level Design
    Please BEFORE PULLING copy your L_Club Level. I accidentally worked on it - I have created L_Club_New - this is where the whole enivronment is.

--- Plugins/FMODStudio/Source/FMODStudio/Public/FMODCallbackHandler.h
+++
@@ -1,5 +0,0 @@
-// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2026.
-
-#pragma once
-
-#include "FMOD/FMODCallbackHandler.h"
--- Plugins/FMODStudio/Source/FMODStudioEditor/Private/FMODStudioEditorPrivatePCH.h
+++
@@ -1,3 +0,0 @@
-// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2026.
-// Placeholder
-// Removed in FMOD 2.04
